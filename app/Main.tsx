ace9639e4c8fdec89d932ca8446aebc5fee458fc
robinyzhang <robinyzhang@tencent.com>
2024-03-23 23:50:05 +0800

    fix blog links

--- app/Main.tsx
+++ app/Main.tsx
@@ -121,7 +121,7 @@ export default function Home({ posts }) {
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-emerald-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="/tags/随笔">
+                  <Link href="/blog">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <span className="pr-3 text-gray-900 dark:text-gray-100">🧑‍🚀 Read</span>

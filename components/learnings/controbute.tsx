fe28f69a02e1f9dd19c220c8742f624466a8f294
robinyzhang <robinyzhang@tencent.com>
2024-04-14 22:16:06 +0800

    add learning first comonment

--- components/learnings/controbute.tsx
+++ components/learnings/controbute.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { ReactElement } from 'react'
 import Link from '../Link'
 import '/css/contribute.css'
 
-const GitHubControbute = () => {
+const getMonthElements = (): ReactElement[] => {
   const months = [
     'Jan',
     'Feb',
@@ -18,26 +19,36 @@ const GitHubControbute = () => {
     'Nov',
     'Dec',
   ]
-  const monthList: Array<HTMLLIElement> = []
+  const monthList: ReactElement[] = []
   months.forEach((month, index) => {
     monthList.push(<li key={index}>{month}</li>)
   })
+  return monthList
+}
 
+const getDayEelents = (): ReactElement[] => {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  const dayList: Array<HTMLLIElement> = []
+  const dayList: ReactElement[] = []
   days.forEach((day, index) => {
     dayList.push(<li key={index}>{day}</li>)
   })
+  return dayList
+}
 
-  const levelList: Array<HTMLLIElement> = []
-  for (let i = 1; i < 365; i++) {
-    const level = Math.floor(Math.random() * 3)
+const getLevelElements = (): ReactElement[] => {
+  const levelList: ReactElement[] = []
+  for (let i = 1; i <= 365; i++) {
+    const level = Math.floor(Math.random() * 3) + 1
     levelList.push(
       <li key={i} data-level={level} className="tooltip">
         <span className="tooltiptext">{level} Points.</span>
       </li>
     )
   }
+  return levelList
+}
+
+const GitHubControbute = () => {
   return (
     <>
       <div className="pt-6 text-center xl:pb-4">
@@ -61,9 +72,9 @@ const GitHubControbute = () => {
           </h2>
         </Link>
         <div className="graph">
-          <ul className="months">{monthList}</ul>
-          <ul className="days">{dayList}</ul>
-          <ul className="squares">{levelList}</ul>
+          <ul className="months">{getMonthElements()}</ul>
+          <ul className="days">{getDayEelents()}</ul>
+          <ul className="squares">{getLevelElements()}</ul>
         </div>
       </div>
     </>

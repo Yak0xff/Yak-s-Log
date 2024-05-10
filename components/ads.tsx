6a486a79d199a3a483c6557edba58c005e00b1be
robinyzhang <robinyzhang@tencent.com>
2024-05-10 16:09:51 +0800

    fix

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,11 +1,14 @@
 function EnableGoogleAdsense() {
-  const head = document.getElementsByTagName('head')[0]
-  const scriptElement = document.createElement(`script`)
-  scriptElement.type = `text/javascript`
-  scriptElement.async = true
-  scriptElement.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6076347298102292`
-  scriptElement.crossOrigin = 'anonymous'
-  head.appendChild(scriptElement)
+  if (typeof document !== 'undefined') {
+    const head = document.getElementsByTagName('head')[0]
+    const scriptElement = document.createElement(`script`)
+    scriptElement.type = `text/javascript`
+    scriptElement.async = true
+    scriptElement.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6076347298102292`
+    scriptElement.crossOrigin = 'anonymous'
+    head.appendChild(scriptElement)
+  }
+  return null // Return null when used as a React component
 }
 
 export default EnableGoogleAdsense

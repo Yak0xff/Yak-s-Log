118f0c4678f100c801119375eedf7c7bb8be7a92
robinyzhang <robinyzhang@tencent.com>
2024-05-17 11:43:22 +0800

    add book notion link

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Demos' },
+  { href: 'https://yak0xff.notion.site/c42bf9148b73456082d08a0959c18c59?pvs=4', title: 'Books' },
   { href: '/about', title: 'About' },
 ]
 
